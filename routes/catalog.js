e2868354901d5feed826e8eaa29179280cba4da6
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-02-08 11:05:37 +0200

    added routes for category parts and all parts

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const categoryController = require('../controllers/categoryController')
+const partController = require('../controllers/partController')
 
 router.get('/', categoryController.index)
 
 router.get('/categories', categoryController.category_list)
+
+router.get('/category/:id', categoryController.category_detail)
+
+router.get('/parts', partController.part_list)
 
 module.exports = router
