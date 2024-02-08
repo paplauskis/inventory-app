c1841c13a44ca1663fa4fcd9ed55598572abfbe1
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-02-08 22:07:10 +0200

    added get and post methods to part update page

--- controllers/partController.js
+++ controllers/partController.js
@@ -5,8 +5,10 @@ const { query, validationResult } = require('express-validator')
 
 exports.part_create_get = asyncHandler(async (req, res, next) => {
   const allCategories = await Category.find({}).sort({ name: 1 }).exec()
+
   res.render('part_form', {
     title: 'Create new part',
+    link: req.url,
     categories: allCategories,
     button_text: 'Submit'
   })
@@ -62,6 +64,67 @@ exports.part_delete_post = asyncHandler(async (req, res, next) => {
     next(error)
   }
 })
+
+exports.part_update_get = asyncHandler(async (req, res) => {
+  const [part, allCategories] = await Promise.all([
+    Part.findById(req.params.id).populate('category'),
+    Category.find({}).exec(),
+  ])
+
+  res.render('part_form', {
+    title: `Update ${part.name}`,
+    link: req.url,
+    part: part,
+    categories: allCategories,
+    part_name: part.name,
+    part_num: part.part_num,
+    part_category: part.category.name,
+    part_price: part.price,
+    part_quantity: part.quantity,
+    button_text: 'Update',
+  })
+})
+
+exports.part_update_post = [
+  query('part_name').trim().escape(),
+  query('part_num').trim().escape(),
+  query('part_category').trim().escape(),
+  query('part_price').trim().escape(),
+  query('part_quantity').trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const part = new Part({
+      name: req.body.part_name,
+      part_num: req.body.part_num,
+      category: req.body.part_category,
+      price: req.body.part_price,
+      quantity: req.body.part_quantity,
+      _id: req.params.id,
+    })
+
+    if (!errors.isEmpty()) {
+      res.render('part_form', {
+        title: `Update ${part.name}`,
+        link: req.url,
+        part: part,
+        categories: allCategories,
+        part_name: part.name,
+        part_num: part.part_num,
+        part_category: part.category.name,
+        part_price: part.price,
+        part_quantity: part.quantity,
+        button_text: 'Update',
+        errors: errors.array(),
+      })
+      return
+    } else {
+      await Part.findByIdAndUpdate(req.params.id, part)
+      res.redirect('/catalog/categories')
+    }
+  })
+]
 
 exports.part_list = asyncHandler(async (req, res, next) => {
   const allParts = await Part.find({})
